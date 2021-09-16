4ff9f850ec4cc857866c595e094d8988bb2f5ce0
Aleksei Milenko <amylenko@mail.ru>
2021-09-16 13:47:33 +0300

    Code added

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        int currentBalance = 2_000_000_000;
-        int incomingTransfer = 500_000_000;
-        int balanceAfterTransfer = currentBalance + incomingTransfer;
-        System.out.println(balanceAfterTransfer);
-    }
+        int ticketCost = 99;
+        int bonusMiles = ticketCost / 20;
+        System.out.println(bonusMiles);
+            }
 }
